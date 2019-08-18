fcbf11f11ff2a693aac3a782eb44aa5e11dc1165
Fangwei Lin <fangweilin@Fangweis-MacBook-Pro.local>
2019-08-18 10:47:53 -0700

    My portfolio

--- src/App.js
+++ src/App.js
@@ -1,26 +1,66 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import Projects from './Projects' ;
+import SocialProfiles from './SocialProfiles';
+import profile from './assets/profile.png'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+class App extends Component {
+    state = {displayBio : false};
+    // constructor() {
+    //     super();
+    //     this.state = {displayBio: false};
+
+    //     console.log('Component this', this);
+    //     this.toggleDisplayBio = this.toggleDisplayBio.bind(this);
+    // }
+
+    toggleDisplayBio = () => {
+        this.setState({displayBio: !this.state.displayBio});
+    }
+
+    // readMore() {
+    //     console.log('readMore this', this);
+    //     this.setState({displayBio : true});
+    // }
+    // showLess() {
+    //     this.setState({displayBio: false});
+    // }
+
+    toggleDisplayBio() {
+        this.setState({displayBio : !this.state.displayBio});
+    }
+
+
+
+  render() {
+    return (
+      <div>
+        <img src={profile} alt='profile' className='profile' />
+        <h1>Hello!</h1>
+        <p>My name is Fangwei Lin.</p>
+        <p>I'm always looking forward to working on meaningful projects.</p>
+        
+        {
+            this.state.displayBio ? (
+        <div>
+            <p>I live in Los Angeles, and code every day.</p>
+            <p>My favorite language are Java and JavaScript, and I think React.js is awesome.</p>
+            <p>Besides coding, I also love music and ramen!</p>
+            <button onClick = {this.toggleDisplayBio}>Show less</button>
+        </div>
+      ): (
+          <div>
+              <button onClick = {this.toggleDisplayBio}>Read more </button>
+              </div>
+      )
+      }
+      <hr />
+      <Projects />
+      <hr />
+      <SocialProfiles />
     </div>
-  );
-}
+    )
+  }
 
+}
 export default App;
