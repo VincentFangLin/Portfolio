9ea02cb354c631d922e2d91b4032f46861f9076b
Fangwei Lin <fangweilin@Fangweis-MacBook-Pro.local>
2019-08-18 11:36:17 -0700

    Updates

---
+++ precache-manifest.ec2d4f62b58fab0e9271d21b0225bc93.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7a408836c4a262d9f361ca1412749f69",
+    "url": "/Portfolio/index.html"
+  },
+  {
+    "revision": "5024938bf654e03be445",
+    "url": "/Portfolio/static/css/main.12385398.chunk.css"
+  },
+  {
+    "revision": "817200ea121e02822930",
+    "url": "/Portfolio/static/js/2.3696b6ce.chunk.js"
+  },
+  {
+    "revision": "5024938bf654e03be445",
+    "url": "/Portfolio/static/js/main.11515519.chunk.js"
+  },
+  {
+    "revision": "02bb7fa0c0195f9aa52b",
+    "url": "/Portfolio/static/js/runtime~main.aeacf694.js"
+  },
+  {
+    "revision": "8262057119c963741b868e0d8dd68dd3",
+    "url": "/Portfolio/static/media/economica-bold.82620571.ttf"
+  },
+  {
+    "revision": "fd99f8e3744a0cf1335282d72cb77f5f",
+    "url": "/Portfolio/static/media/email_icon.fd99f8e3.png"
+  },
+  {
+    "revision": "e2a6288611c5c203fdca4761ca408acf",
+    "url": "/Portfolio/static/media/github_icon.e2a62886.png"
+  },
+  {
+    "revision": "56ab00e848077ea3fec713da56e69681",
+    "url": "/Portfolio/static/media/linkedin_icon.56ab00e8.png"
+  },
+  {
+    "revision": "9f0da1872b96248f9656a1f92cf29af0",
+    "url": "/Portfolio/static/media/profile.9f0da187.png"
+  },
+  {
+    "revision": "bf56edc6c7090e92662e37a9b9ed2aea",
+    "url": "/Portfolio/static/media/project1.bf56edc6.png"
+  },
+  {
+    "revision": "f7e79c7d61c8e4a45847337bcec527e6",
+    "url": "/Portfolio/static/media/project2.f7e79c7d.png"
+  },
+  {
+    "revision": "7bded960f9e6263b998eb4529af6e041",
+    "url": "/Portfolio/static/media/project3.7bded960.png"
+  },
+  {
+    "revision": "cf4449c1884598b292630aa9f4007c19",
+    "url": "/Portfolio/static/media/roboto-condensed.light.cf4449c1.ttf"
+  },
+  {
+    "revision": "512536cb3a4583052bd81fbf9760521f",
+    "url": "/Portfolio/static/media/twitter_icon.512536cb.png"
+  }
+]);
